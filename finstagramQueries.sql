c0e62ed49b9279f2a7371b4a212a08b406fa311b
Brian Shin <bsshinple@gmail.com>
2019-11-26 21:44:15 -0500

    query edited

--- finstagramQueries.sql
+++ finstagramQueries.sql
@@ -1,6 +1,24 @@
-# 3.1 Query to find photoIDs of photos that are visible to the user whose username
+-- 3.1 Query to find photoIDs of photos that are visible to the user whose username
 
-SELECT photoID
-FROM Photo JOIN Person ON (Photo.photoPoster = Person.username)
-WHERE allFollowers = True AND username in (SELECT username_follower FROM Follow WHERE username_followed = Photo.photoPoster)
-    OR username in (SELECT member_username FROM BelongTo NATURAL JOIN SharedWith WHERE SharedWith.photoID = Photo.photoID)
+SELECT
+    photoID
+FROM
+    Photo
+JOIN Person ON(
+        Photo.photoPoster = Person.username
+    )
+WHERE
+    allFollowers = TRUE AND username IN(
+    SELECT
+        username_follower
+    FROM
+        Follow
+    WHERE
+        username_followed = Photo.photoPoster
+) OR username IN(
+    SELECT
+        member_username
+    FROM
+        BelongTo
+    NATURAL JOIN SharedWith WHERE SharedWith.photoID = Photo.photoID
+)
